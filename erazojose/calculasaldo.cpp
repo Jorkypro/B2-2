87b9328b5993e1b16038f3b11a740ff623195ff0
jose <erazoleonjoseluis@gmail.com>
2022-10-26 18:28:16 -0500

    calcula saldo

---
+++ erazojose/calculasaldo.cpp
@@ -0,0 +1,25 @@
+//Programa de erazo 
+ #include<iostream>  
+  using namespace std;  
+  int main()  
+    
+  {  
+          float x,s=0;  
+          int i=0,l;  
+          cout<<"ingrese cantidad de egreso (l):  ";  
+                  cin>>l;  
+          cout<<"ingrese saldo inicial (s):  ";  
+          cin>>s;  
+    
+          do{  
+            
+          cout<<"ingrese egreso (x):  ";  
+          cin>>x;  
+          i=i+1;  
+          s=s+x;  
+          }while(i<l);  
+          cout<<"el saldo final es:  "<<s<<endl;  
+          return 0;  
+    
+  }
+
